3f282fac805ff0dda80663b89e147f5d447d7644
Marierlin Higuerey <67464628+Marierlinh@users.noreply.github.com>
2022-11-06 12:54:37 -0500

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,14 @@
+const localQuotes = [
+    {
+        text:'Be yourself; everyone else is already taken.',
+        author:' Oscar Wilde',
+    },
+    {
+        text:'Be the change that you wish to see in the world.',
+        author:'Mahatma Gandhi',
+    },
+    {
+        text:'You only live once, but if you do it right, once is enough.',
+        author:'Mae West',
+    }
+]
